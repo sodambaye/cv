d00e5b4cf8c4ca2811a2de04a46cdbd7df164beb
sodambaye <mbayesoda789@gmail.com>
2026-02-27 15:09:04 +0000

    Add files via upload

---
+++ cv.py
@@ -0,0 +1,69 @@
+import streamlit as st 
+st.title ("SODA MBAYE")
+st.sidebar.title("PROFIL")
+st.sidebar.markdown("Technicienne Supérieure en Géomatique")
+st.sidebar.markdown("""
+
+**Adresse** : DAKAR, SICAPE LIBERTE
+
+**Email** :
+[mbayesoda789@gmail.com](mailto:mbayesoda789@gmail.com)
+
+""")
+
+st.sidebar.markdown("---")
+st.sidebar.markdown(
+    """
+    <p style="color:black; font-size:1em; line-height:1.2em; text-align:center;">
+       Merci de me contacter par email ;
+       je traite les messages rapidement
+    </p>
+    """,
+    unsafe_allow_html=True)
+
+st.subheader("🎓**FORMATION**")
+
+st.markdown("""
+* 2025 2026 BTS en Geomatique (Formation en cours)
+* 2023 2024  Baccalauréat Série L2 """)
+
+
+st.subheader ("🎯**COMPETENCES**")
+st.markdown("""
+* Géo-référencement
+* Notions en Python
+* Maîtrise des techniques de levés topographiques
+* Traitement sur MicroStation
+* Conception de plans sur AutoCAD
+* Numérisation
+* gestion de bases de données
+* Conception et mise en page de cartes thématiques
+* Pilotage Drone
+* Teledetection """)
+
+st.subheader (" 💻 **Logiciels maîtrisés**")
+st.markdown("""
+* QGIS
+* AutoCAD 
+* Python
+* ArcGIS
+* Excel
+* Pix4D
+* Erdas
+* PowerPoint
+* PostgreSQL
+""")
+st.subheader (" 💼EXPÉRIENCE  PROFESSIONNELLES")
+st.markdown("""
+
+* Stage au service cadastral de Louga
+* Participation à des projets cartographiques académiques""")
+
+st.subheader("🚀**PERSPECTIVES**")
+st.markdown("""
+* Approfondir mes compétences en géomatique et en systèmes d'information géographique
+* Me spécialiser en cadastre et aménagement du territoire
+* Développer mes compétences en bases de données spatiales (PostgreSQL / PostGIS)
+* Poursuivre mes études vers une Licence professionnelle en géomatique""")
+
+
